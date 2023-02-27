d17b1f3d35174649155aa139f91148a3dd96512e
CaioRuizdeSouza <caio.souza@sptech.school>
2023-02-27 14:13:19 -0300

    Curso Java Atualizado

---
+++ Curso Java/curso-java-basico/src/main/java/orientacaoaobjetos2/Carro2.java
@@ -0,0 +1,25 @@
+package orientacaoaobjetos2;
+
+public class Carro2 {
+    
+    String marca;
+    String modelo;
+    int velocidadeMaxima;
+    int taxaAceleracao;
+    double quilometrosPorLitro;
+    
+    void acelerar() {
+        System.out.println("Acelerando......");
+    }
+    
+    void frear() {
+        System.out.println("Freando.....");
+    }
+    
+    void virar() {
+        System.out.println("Virando.......");
+    }
+    
+    
+    
+}
